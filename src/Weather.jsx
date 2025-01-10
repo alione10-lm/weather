a44a7153160e6930f3699c17ebc4482d38277ff6
ali1 <aliouaneoussama89@gmail.com>
2025-01-10 23:36:40 +0100

    second commit

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { convertToFlag, getWeatherIcon } from "./services/helpers";
+import { LuSearch } from "react-icons/lu";
 
 function Weather() {
   const [weather, setWeather] = useState([]);
@@ -7,50 +8,87 @@ function Weather() {
   const [location, setLocation] = useState("");
   const [Country, setCountry] = useState();
 
-  useEffect(
-    function () {
-      async function fetchWeather() {
-        if (location.length < 3) return setWeather(() => []);
-        setisLoading(true);
-        try {
-          const geoRes = await fetch(
-            `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-          );
-          const geoData = await geoRes.json();
+  // useEffect(
+  //   function () {
+  //     async function fetchWeather() {
+  //       if (location.length < 3) return setWeather(() => []);
+  //       setisLoading(true);
+  //       try {
+  //         const geoRes = await fetch(
+  //           `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+  //         );
+  //         const geoData = await geoRes.json();
 
-          if (!geoData.results) throw new Error("Location not found");
+  //         if (!geoData.results) throw new Error("Location not found");
 
-          const { latitude, longitude, timezone, name, country_code } =
-            geoData.results.at(0);
+  //         const { latitude, longitude, timezone, name, country_code } =
+  //           geoData.results.at(0);
 
-          const weatherRes = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
-          );
-          const weatherData = await weatherRes.json();
-          setCountry(() => `${name} ${convertToFlag(country_code)}`);
-          setWeather(weatherData.daily);
-        } catch (err) {
-          console.error(err);
-        } finally {
-          setisLoading(false);
-        }
+  //         const weatherRes = await fetch(
+  //           `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+  //         );
+  //         const weatherData = await weatherRes.json();
+  //         setCountry(() => `${name} ${convertToFlag(country_code)}`);
+  //         setWeather(weatherData.daily);
+  //       } catch (err) {
+  //         console.error(err);
+  //       } finally {
+  //         setisLoading(false);
+  //       }
+  //     }
+
+  //     fetchWeather();
+  //   },
+  //   [location]
+  // );
+
+  function HandleSubmit(e) {
+    e.preventDefault();
+
+    async function fetchWeather() {
+      if (location.length < 3) return setWeather(() => []);
+      setisLoading(true);
+      try {
+        const geoRes = await fetch(
+          `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+        );
+        const geoData = await geoRes.json();
+
+        if (!geoData.results) throw new Error("Location not found");
+
+        const { latitude, longitude, timezone, name, country_code } =
+          geoData.results.at(0);
+
+        const weatherRes = await fetch(
+          `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+        );
+        const weatherData = await weatherRes.json();
+        setCountry(() => `${name} ${convertToFlag(country_code)}`);
+        setWeather(weatherData.daily);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setisLoading(false);
       }
+    }
 
-      fetchWeather();
-    },
-    [location]
-  );
-
+    fetchWeather();
+  }
   return (
     <div className="flex flex-col items-center  gap-4 ">
       <header className="z-50 bg-[url('/hero.jpeg')]  bg-no-repeat bg-cover bg-center w-full h-56 rounded-md flex flex-col items-center">
-        <input
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          type="text"
-          placeholder="search..."
-          className="text-indigo-500 bg-transparent backdrop-blur-xl rounded-lg border-none  focus:outline-none focus:ring-2 focus:ring-indigo-500 px-4  transition-all placeholder:text-indigo-500 duration-300 py-2 my-4"
-        />
+        <form onSubmit={HandleSubmit} className="flex items-center gap-1">
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            type="text"
+            placeholder="city ..."
+            className="text-indigo-500 bg-transparent backdrop-blur-xl rounded-lg border-none  focus:outline-none focus:ring-2 focus:ring-indigo-600 px-4  transition-all placeholder:text-indigo-500 duration-300 py-2 my-4"
+          />
+          <button className="bg-indigo-500 border-none focus:border-none focus:outline-none hover:bg-indigo-600 transition-all duration-300 focus:ring focus:ring-indigo-600  text-indigo-50 font-medium block px-4 py-2 rounded-lg">
+            search
+          </button>
+        </form>
         {Country && (
           <h1 className="text-slate-100 font-medium text-2xl">{Country}</h1>
         )}
@@ -84,7 +122,11 @@ function Weather() {
           ))
         )}
       </header>
-
+      {weather.length === 0 && (
+        <h1 className="text-indigo-500 text-center px-4 font-medium mt-10 text-2xl">
+          Discover the weather in every city you go 😀
+        </h1>
+      )}
       {isLoading ? (
         <Spinner />
       ) : (
